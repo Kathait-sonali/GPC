1d8fa0f019cff95f698f43a88e7cfebee0c75596
Kathait-sonali <sonali.kathait@tcs.com>
2025-07-25 13:08:59 +0530

    Add files via upload

---
+++ backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket                      = "gpc-oci-n-platform-tf"
+    region                      = "us-ashburn-1"
+    key                         = "non-prod/network/default.tfstate"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
+    skip_s3_checksum            = true
+    skip_metadata_api_check     = true
+    endpoints = {
+      s3 = "https://idxkhihnubxu.compat.objectstorage.us-ashburn-1.oraclecloud.com"
+    }
+  }
+}
