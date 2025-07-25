c0b42e31d67ad07dec8fa5aff4f21b546e71a00a
Kathait-sonali <sonali.kathait@tcs.com>
2025-07-25 13:48:40 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -14,3 +14,19 @@ terraform {
     }
   }
 }
+terraform {
+  backend "s3" {
+    bucket                      = "gpc-oci-n-network-tf"
+    region                      = "us-ashburn-1"
+    key                         = "non-prod/network/default.tfstate"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
+    skip_s3_checksum            = true
+    skip_metadata_api_check     = true
+    endpoints = {
+      s3 = "https://idxkhihnubxu.compat.objectstorage.us-ashburn-1.oraclecloud.com"
+    }
+  }
+}
